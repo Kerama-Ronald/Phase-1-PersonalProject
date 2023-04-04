f2d4a2b9aa2bff7090df53f41ed6e1f50207dee4
Ronald Kerama <keramaronald11@gmail.com>
2023-04-04 14:08:37 +0300

    Fourth commit

--- first.js
+++ first.js
@@ -1,16 +1,25 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); 
+function fectchData(url){
+    
+    fetch(url).then((response) => {
+        response.json().then(res => {
+            console.log(res)
+            renderData(res)
+        })
+        
+    }).catch((error) => console.error(error))
 }
+
+function home(){
+    fectchData('http://localhost:3000/characters')
+}
+const form = document.getElementById("myForm");
+form.addEventListener("submit", handleSubmit);function handleSubmit(event) {
+  event.preventDefault(); // Prevent default form submission behavior  // Get form values
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;  // Do something with the form data
+  console.log(name, email, message);  // Reset the form after submission
+  form.reset();
+}
+New
+2:06
