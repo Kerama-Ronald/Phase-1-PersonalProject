65cde8a979fc0b202b57ecca8ca4665ab4c81297
Ronald Kerama <keramaronald11@gmail.com>
2023-04-04 16:18:32 +0300

    not yet

--- first.js
+++ first.js
@@ -1,10 +1,26 @@
-const form = document.getElementById("myForm");
-form.addEventListener("submit", handleSubmit);function handleSubmit(event) {
-  event.preventDefault(); // Prevent default form submission behavior  // Get form values
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;  // Do something with the form data
-  console.log(name, email, message);  // Reset the form after submission
-  form.reset();
-}
-
+// Fetch the data from the JSON file
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Loop through the cryptocurrencies and create a flipping card for each one
+    data.cryptoCurrencies.forEach(cryptoCurrency => {
+      const card = document.createElement('div');
+      card.classList.add('col-md-4');
+      card.innerHTML = `
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src="${cryptoCurrency.image}" alt="${cryptoCurrency.name}" />
+            </div>
+            <div class="flip-card-back">
+              <h2>${cryptoCurrency.name}</h2>
+              <p>${cryptoCurrency.description}</p>
+              <p><strong>Year Established:</strong> ${cryptoCurrency.yearEstablished}</p>
+              <p><strong>Founders:</strong> ${cryptoCurrency.founders.join(', ')}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      document.querySelector('.row').appendChild(card);
+    });
+  });
