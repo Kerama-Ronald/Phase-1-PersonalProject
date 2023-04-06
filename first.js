2685a230db6aa1198f73588b4c99a2ae597cf184
Ronald Kerama <keramaronald11@gmail.com>
2023-04-06 06:02:17 +0300

    reviews

--- first.js
+++ first.js
@@ -1,26 +1,33 @@
-// Fetch the data from the JSON file
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Loop through the cryptocurrencies and create a flipping card for each one
-    data.cryptoCurrencies.forEach(cryptoCurrency => {
-      const card = document.createElement('div');
-      card.classList.add('col-md-4');
-      card.innerHTML = `
-        <div class="flip-card">
-          <div class="flip-card-inner">
-            <div class="flip-card-front">
-              <img src="${cryptoCurrency.image}" alt="${cryptoCurrency.name}" />
-            </div>
-            <div class="flip-card-back">
-              <h2>${cryptoCurrency.name}</h2>
-              <p>${cryptoCurrency.description}</p>
-              <p><strong>Year Established:</strong> ${cryptoCurrency.yearEstablished}</p>
-              <p><strong>Founders:</strong> ${cryptoCurrency.founders.join(', ')}</p>
-            </div>
-          </div>
-        </div>
-      `;
-      document.querySelector('.row').appendChild(card);
-    });
-  });
+
+
+   // get form elements
+const form = document.querySelector("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+
+// form submission handler
+form.addEventListener("submit", function(event) {
+  // prevent default form submission behavior
+  event.preventDefault();
+
+  // validate form inputs
+  if (nameInput.checkValidity() && emailInput.checkValidity() && messageInput.checkValidity()) {
+    // create and send form data
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "path/to/server", true);
+    xhr.send(formData);
+
+    // reset form
+    form.reset();
+  }
+});
+
+// form reset handler
+form.addEventListener("reset", function(event) {
+  // clear form validation messages
+  nameInput.setCustomValidity("");
+  emailInput.setCustomValidity("");
+  messageInput.setCustomValidity("");
+});
